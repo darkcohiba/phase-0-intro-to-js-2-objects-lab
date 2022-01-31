18b4aa8c965f611e31a8eb0f75939953baabc658
Sam <samuel.waters@gmail.com>
2022-01-30 18:44:03 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "12 Broadway",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return [employee], {
+        ...employee,
+        [key]: value,
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const cloneEmployee = Object.assign({}, employee);
+    delete cloneEmployee[key];
+    return cloneEmployee;
+}
+
+function  destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
